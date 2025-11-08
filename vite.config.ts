e0127e6e5fbe36d6ac8ed380002cca205a6f0e4d
andyvin25 <joviandy2020@gmail.com>
2025-11-08 16:55:57 +0700

    fixed dependancy

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,58 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import vitePluginBundleObfuscator from 'vite-plugin-bundle-obfuscator';
+import type { ObfuscatorOptions } from 'javascript-obfuscator';
 
+// https://vite.dev/config/
 
-const minimizeObfuscatorConfig = {
+const allObfuscatorConfig: ObfuscatorOptions = {
+  excludes: [],
+  enable: true,
+  log: true,
   autoExcludeNodeModules: true,
   // autoExcludeNodeModules: { enable: true, manualChunks: ['vue'] }
   threadPool: true,
   // threadPool: { enable: true, size: 4 }
+  options: {
+    compact: true,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    deadCodeInjection: false,
+    debugProtection: false,
+    debugProtectionInterval: 0,
+    disableConsoleOutput: false,
+    identifierNamesGenerator: 'hexadecimal',
+    log: false,
+    numbersToExpressions: false,
+    renameGlobals: false,
+    selfDefending: true,
+    simplify: true,
+    splitStrings: false,
+    ignoreImports: true,
+    stringArray: true,
+    stringArrayCallsTransform: true,
+    stringArrayCallsTransformThreshold: 0.5,
+    stringArrayEncoding: [],
+    stringArrayIndexShift: true,
+    stringArrayRotate: true,
+    stringArrayShuffle: true,
+    stringArrayWrappersCount: 1,
+    stringArrayWrappersChainedCalls: true,
+    stringArrayWrappersParametersMaxCount: 2,
+    stringArrayWrappersType: 'variable',
+    stringArrayThreshold: 0.75,
+    unicodeEscapeSequence: false,
+  }
 };
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
-    vitePluginBundleObfuscator(minimizeObfuscatorConfig)
+    // vitePluginBundleObfuscator(allObfuscatorConfig)
+    vitePluginBundleObfuscator(allObfuscatorConfig)
   ],
   resolve: {
     alias: {
